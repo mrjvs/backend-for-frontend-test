127526d46a8182b785450b5e7b17c69ba941d208
William Oldham <william.oldham@middlegatesoftware.com>
2024-06-05 15:31:10 +0100

    Add seeding for posts

--- src/modules/prisma/seeds/index.ts
+++ src/modules/prisma/seeds/index.ts
@@ -1,3 +1,37 @@
+import { type Prisma } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import slugify from 'slugify';
+import { prisma } from '@/modules/prisma';
+import { getId } from '@/utils/get-id';
+
+function createFakePost(): Prisma.PostCreateInput {
+  const title = faker.word.words(3);
+  const slug = slugify(title);
+  const content = `${faker.lorem.sentence({ min: 10, max: 10 })}
+
+    # ${title}
+    ${faker.lorem.paragraphs(3)}
+
+    ## ${faker.airline.airline().name}
+    ${faker.lorem.paragraphs(3)}
+
+    ---
+
+    ${faker.lorem.paragraphs(2)}
+  `;
+
+  return {
+    title,
+    slug,
+    content,
+    id: getId('pst'),
+  };
+}
+
 export async function runSeeding() {
-  // There is nothing to seed :)
+  await prisma.post.createMany({
+    data: faker.helpers.multiple(createFakePost, {
+      count: 20,
+    }),
+  });
 }
